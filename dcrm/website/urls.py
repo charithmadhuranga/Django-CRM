e7683a027c1d860cffe498f703642e22697bf941
nikhuge <nikhuge@gmail.com>
2025-01-11 16:01:10 +0530

    all functions completed

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path("login/",views.login_user,name="login"),
     path("logout/",views.logout_user,name="logout"),
     path("signup/",views.signup_user,name="signup"),
+    path("record/<int:pk>/",views.customer_record,name="record"),
+    path("delete/<int:pk>/",views.delete_record,name="delete"),
+    path("add_record/",views.add_record,name="add_record"),
+    path("update_record/<int:pk>/",views.update_record,name="update_record"),
 ]
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -3,6 +3,7 @@ from django.contrib.messages.context_processors import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Record
 
 # Create your views here.
 def home(request):
@@ -12,7 +13,8 @@ def home(request):
 def crm(request):
     if not request.user.is_authenticated:
         return redirect('login')
-    return render(request,'crm.html',{})
+    records = Record.objects.all()
+    return render(request,'crm.html',{ 'records':records })
 
 def login_user(request):
 
@@ -76,3 +78,55 @@ def signup_user(request):
         return redirect('login')  # Replace 'login' with your login page URL name
 
     return render(request, 'signup.html')
+
+
+def customer_record(request,pk):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    customer_record = Record.objects.get(id=pk)
+    return render(request,'record.html',{'customer_record':customer_record})
+
+def delete_record(request,pk):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    record = Record.objects.get(id=pk)
+    record.delete()
+    messages.success(request,'Record Deleted Successfully')
+    return redirect('crm')
+
+def add_record(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    if request.method == 'POST':
+        first_name = request.POST.get('firstName')
+        last_name = request.POST.get('lastName')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        address = request.POST.get('address')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zipcode = request.POST.get('zipcode')
+
+        recordme = Record.objects.create(first_name=first_name, last_name=last_name, email=email, phone=phone, address=address, city=city, state=state, zipcode=zipcode)
+        recordme.save()
+        messages.success(request,'Record Added Successfully')
+
+    return render(request,'add_record.html')
+
+def update_record(request,pk):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    customer_record = Record.objects.get(id=pk)
+    if request.method == 'POST':
+        customer_record.first_name = request.POST.get('firstName')
+        customer_record.last_name = request.POST.get('lastName')
+        customer_record.email = request.POST.get('email')
+        customer_record.phone = request.POST.get('phone')
+        customer_record.address = request.POST.get('address')
+        customer_record.city = request.POST.get('city')
+        customer_record.state = request.POST.get('state')
+        customer_record.zipcode = request.POST.get('zipcode')
+        customer_record.save()
+        messages.success(request,'Record Updated Successfully')
+        return redirect('crm')
+    return render(request,'update_record.html',{'customer_record': customer_record})
